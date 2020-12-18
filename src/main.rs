8a5927b1f69398c9902b03f5aebd919e9d8d1a56
kino-ma <kino.ma.ms@gmail.com>
2020-12-18 18:49:00 +0900

    [fix] unused variable warning

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use qcure::token::{Code};
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
     let code = String::from("");
-    let tokens = Code::from(&code)?;
+    let _tokens = Code::from(&code)?;
 
     Ok(())
 }
