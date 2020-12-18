33a462d66767b925a91a0a92b3894787d31fa202
kino-ma <kino.ma.ms@gmail.com>
2020-12-18 17:54:01 +0900

    [change] `main` return type to dyn

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use qcure::token::{Code};
 
-fn main() -> Result<(), Box<std::error::Error>>{
+fn main() -> Result<(), Box<dyn std::error::Error>>{
     let code = String::from("");
     let tokens = Code::from(&code)?;
 
