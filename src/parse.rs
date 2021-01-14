6ba48f7c6b460b44bc5c88663243db9387108ab5
kino-ma <kino.ma.ms@gmail.com>
2021-01-14 19:27:16 +0900

    [change] type names "s/$NAME/&_/g"

--- src/parse.rs
+++ src/parse.rs
@@ -44,7 +44,7 @@ fn expect<'a, I: std::iter::Iterator<Item = &'a Token>>(it: &mut I, kind: Option
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Statement {
-    Assign { prefix: Option<AssignPrefix>, ident: String, expr: Expr },
+    Assign { prefix: Option<AssignPrefix>, ident: String, expr: Expr_ },
 }
 
 impl Statement {
@@ -87,7 +87,7 @@ impl Statement {
 
         expect(&mut it, Some(TK::Symbol), Some(":="))?;
 
-        expr = Expr::new(&mut it.collect())?;
+        expr = Expr_::new(&mut it.collect())?;
 
         Ok(Self::Assign{
             prefix,
@@ -103,13 +103,13 @@ pub enum AssignPrefix {
 }
 
 #[derive(Debug, PartialEq, Clone)]
-pub enum Expr {
-    Term(Term),
-    FuncApplication(FuncApplicationOp),
+pub enum Expr_ {
+    Term(Term_),
+    FuncApplication(FuncApplicationOp_),
     Bind,
 }
 
-impl Expr {
+impl Expr_ {
     pub fn new(v: &mut Vec<&Token>) -> Result<Self> {
         let expr = Self::term(v)
             .or(Self::func_app(v))?;
@@ -120,35 +120,36 @@ impl Expr {
     }
 
     pub fn term(v: &mut Vec<&Token>) -> Result<Self> {
-        Term::new(v)
+        Term_::new(v)
             .map(Self::Term)
     }
 
     pub fn func_app(v: &mut Vec<&Token>) -> Result<Self> {
-        FuncApplication::new(v)
-            .map(Self::FuncApplicationOp)
+        FuncApplicationOp_::new(v)
+            .map(Self::FuncApplication)
     }
 }
 
 #[derive(Debug, PartialEq, Clone)]
-pub enum FuncApplicationOp {
-    FuncApplication(FuncApplication),
-    UnaryOp { op: UnaryOp, arg: Box<FuncApplication> },
-    BinaryOpL { op: BinaryOpL, arg1: Box<FuncApplicationOp>, arg2: Box<FuncApplicationOp> },
-    BinaryOpR { op: BinaryOpR, arg1: Box<FuncApplicationOp>, arg2: Box<FuncApplicationOp> },
+pub enum FuncApplicationOp_ {
+    FuncApplication(FuncApplication_),
+    UnaryOp { op: UnaryOp_, arg: Box<FuncApplication_> },
+    BinaryOpL { op: BinaryOpL_, arg1: Box<FuncApplicationOp_>, arg2: Box<FuncApplicationOp_> },
+    BinaryOpR { op: BinaryOpR_, arg1: Box<FuncApplicationOp_>, arg2: Box<FuncApplicationOp_> },
 }
 
 #[derive(Debug, PartialEq, Clone)]
-pub enum FuncApplication {
-    Term(Term),
-    Normal { op: Box<FuncApplication>, args: Vec<Term> },
+pub enum FuncApplication_ {
+    Term(Term_),
+    Normal { op: Box<FuncApplication_>, args: Vec<Term_> },
 }
+use FuncApplication_::*;
 
-type UnaryOp = Term;
-type BinaryOpL = Term;
-type BinaryOpR = Term;
+type UnaryOp_ = Term_;
+type BinaryOpL_ = Term_;
+type BinaryOpR_ = Term_;
 
-impl FuncApplication {
+impl FuncApplication_ {
     pub fn new(v: &mut Vec<&Token>) -> Result<Self> {
         let first = v[0];
         
@@ -158,18 +159,18 @@ impl FuncApplication {
 }
 
 #[derive(Debug, PartialEq, Clone)]
-pub enum Term {
+pub enum Term_ {
     Identifier(String),
     Operator(String),
     Literal(LiteralValue),
-    Expr(Box<Expr>),
-    Block { stmts: Vec<Statement>, rt_keyword: Option<()>, expr: Box<Expr> },
+    Expr(Box<Expr_>),
+    Block { stmts: Vec<Statement>, rt_keyword: Option<()>, expr: Box<Expr_> },
     Selection,
     Repetation,
 }
-use Term::*;
+use Term_::*;
 
-impl Term {
+impl Term_ {
     pub fn new(tokens: &mut Vec<&Token>) -> Result<Self> {
         let tk = tokens[0];
         match tk.k {
@@ -213,7 +214,7 @@ use LiteralValue::*;
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct ExprStack{
-    stack: [Vec<Term>; 10],
+    stack: [Vec<Term_>; 10],
 }
 
 impl ExprStack {
@@ -222,7 +223,7 @@ impl ExprStack {
         Self::from(stack)
     }
 
-    pub fn from(stack: [Vec<Term>; 10]) -> Self {
+    pub fn from(stack: [Vec<Term_>; 10]) -> Self {
         Self { stack }
     }
 
@@ -232,11 +233,11 @@ impl ExprStack {
         Self::from(stack)
     }
 
-    pub fn push(&mut self, term: Term) {
+    pub fn push(&mut self, term: Term_) {
         self.stack[term.prior()].push(term);
     }
 
-    pub fn pop(&mut self) -> Option<Term> {
+    pub fn pop(&mut self) -> Option<Term_> {
         let mut i = 0;
         while self[i].is_empty() {
             i += 1;
@@ -250,7 +251,7 @@ impl ExprStack {
             .all(|stack| stack.is_empty())
     }
 
-    pub fn empty_stack() -> [Vec<Term>; 10] {
+    pub fn empty_stack() -> [Vec<Term_>; 10] {
         [
             Vec::new(),
             Vec::new(),
@@ -266,7 +267,7 @@ impl ExprStack {
     }
 }
 
-impl<I: std::slice::SliceIndex<[Vec<Term>]>> std::ops::Index<I> for ExprStack {
+impl<I: std::slice::SliceIndex<[Vec<Term_>]>> std::ops::Index<I> for ExprStack {
     type Output = I::Output;
 
     fn index(&self, index: I) -> &Self::Output {
@@ -274,7 +275,7 @@ impl<I: std::slice::SliceIndex<[Vec<Term>]>> std::ops::Index<I> for ExprStack {
     }
 }
 
-impl<I: std::slice::SliceIndex<[Vec<Term>]>> std::ops::IndexMut<I> for ExprStack {
+impl<I: std::slice::SliceIndex<[Vec<Term_>]>> std::ops::IndexMut<I> for ExprStack {
     fn index_mut(&mut self, index: I) -> &mut Self::Output {
         &mut self.stack[index]
     }
@@ -325,7 +326,7 @@ mod tests {
 
         let mut iter = code.iter();
 
-        let expr = Expr::new(&mut vec![&Token::new("1".to_string(), TK::Numeric)]).unwrap();
+        let expr = Expr_::new(&mut vec![&Token::new("1".to_string(), TK::Numeric)]).unwrap();
         let expect = Statement::Assign { prefix: None, ident: "hoge".to_string(), expr };
         let actual = Statement::new(&mut iter).unwrap();
 
@@ -337,9 +338,9 @@ mod tests {
         let src = complex_expr_src();
         let tokens = Code::from(src).expect("failed to tokenize").tokens;
 
-        let expect: Expr = complex_expr();
+        let expect: Expr_ = complex_expr();
 
-        let actual = Expr::new(&mut tokens.iter().collect()).expect("failed to parse expr");
+        let actual = Expr_::new(&mut tokens.iter().collect()).expect("failed to parse expr");
         assert_eq!(expect, actual);
     }
 
@@ -347,35 +348,35 @@ mod tests {
         "f 1 + 2 * (-3 + 4)"
     }
 
-    fn complex_expr() -> Expr {
-        Expr::FuncApplication (
-            FuncApplicationOp::BinaryOpL {
-                op: Term::Operator("+".to_string()),
-                arg1: Box::new(FuncApplicationOp::FuncApplication(FuncApplication::Normal{
-                        op: Box::new(FuncApplication::Term(
-                            Term::Identifier("f".to_string())
+    fn complex_expr() -> Expr_ {
+        Expr_::FuncApplication (
+            FuncApplicationOp_::BinaryOpL {
+                op: Term_::Operator("+".to_string()),
+                arg1: Box::new(FuncApplicationOp_::FuncApplication(FuncApplication_::Normal{
+                        op: Box::new(FuncApplication_::Term(
+                            Term_::Identifier("f".to_string())
                         )),
-                        args: vec![Term::Literal(NumericLiteral(1))]
+                        args: vec![Term_::Literal(NumericLiteral(1))]
                     })
                 ),
-                arg2: Box::new(FuncApplicationOp::BinaryOpL {
-                    op: Term::Operator("*".to_string()),
-                    arg1: Box::new(FuncApplicationOp::FuncApplication(FuncApplication::Term(Term::Literal(LiteralValue::NumericLiteral(
+                arg2: Box::new(FuncApplicationOp_::BinaryOpL {
+                    op: Term_::Operator("*".to_string()),
+                    arg1: Box::new(FuncApplicationOp_::FuncApplication(FuncApplication_::Term(Term_::Literal(LiteralValue::NumericLiteral(
                         2
                     ))))),
-                    arg2: Box::new(FuncApplicationOp::BinaryOpL {
-                        op: Term::Operator(
+                    arg2: Box::new(FuncApplicationOp_::BinaryOpL {
+                        op: Term_::Operator(
                             "+".to_string()
                         ),
-                        arg1: Box::new(FuncApplicationOp::UnaryOp{
-                            op: Term::Operator(
+                        arg1: Box::new(FuncApplicationOp_::UnaryOp{
+                            op: Term_::Operator(
                                 "-".to_string()
                             ),
-                            arg: Box::new(FuncApplication::Term(Term::Literal(LiteralValue::NumericLiteral(
+                            arg: Box::new(FuncApplication_::Term(Term_::Literal(LiteralValue::NumericLiteral(
                                 3
                             ))))
                         }),
-                        arg2: Box::new(FuncApplicationOp::FuncApplication(FuncApplication::Term(Term::Literal(LiteralValue::NumericLiteral(
+                        arg2: Box::new(FuncApplicationOp_::FuncApplication(FuncApplication_::Term(Term_::Literal(LiteralValue::NumericLiteral(
                             4
                         )))))
                     })
